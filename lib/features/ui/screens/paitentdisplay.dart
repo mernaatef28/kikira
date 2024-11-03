fab29679d63932620e5a9b8071bb1f4dad51f251
merna atef <142946454+mernaatef28@users.noreply.github.com>
2024-11-03 04:57:33 +0200

    paitient chart class out

--- lib/features/ui/screens/paitentdisplay.dart
+++ lib/features/ui/screens/paitentdisplay.dart
@@ -2,6 +2,7 @@ import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:kikira/core/api/apiservice.dart';
 import 'package:kikira/core/theming/colors.dart';
+import 'package:kikira/features/ui/widgets/chartpatient.dart';
 import 'package:kikira/features/ui/widgets/dataCard.dart';
 import 'package:kikira/kiki_app.dart';
 
@@ -94,51 +95,6 @@ class _PaitentdisplayState extends State<Paitentdisplay> {
               ),
             ],
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class LineChartWidget extends StatelessWidget {
-  final List<PatientDto> patientData;
-
-  const LineChartWidget({Key? key, required this.patientData}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    List<FlSpot> spots = [];
-
-    // Create FlSpot for the chart
-    for (var patient in patientData) {
-      DateTime date = DateTime.parse(patient.date); // Adjust according to your date format
-      double yValue = double.parse(patient.sugarPercentage.toString());
-      spots.add(FlSpot(date.millisecondsSinceEpoch.toDouble(), yValue));
-    }
-
-    return LineChart(
-      LineChartData(
-        titlesData: FlTitlesData(
-          leftTitles: AxisTitles(
-            sideTitles: SideTitles(showTitles: true, reservedSize: 40),
-          ),
-          bottomTitles: AxisTitles(
-            sideTitles: SideTitles(showTitles: true, reservedSize: 40),
-          ),
-        ),
-        gridData: FlGridData(show: true),
-        lineBarsData: [
-          LineChartBarData(
-            spots: spots,
-            isCurved: true,
-            color: Colors.blue,
-            dotData: FlDotData(show: true),
-            belowBarData: BarAreaData(show: false),
-          ),
-        ],
-        borderData: FlBorderData(
-          show: true,
-          border: Border.all(color: Colors.black, width: 1),
         ),
       ),
     );
